713b659b1629ccd25a8c8695a41bbb53f9f156c9
SahilThakur198 <sahil.aarakh@gmail.com>
2026-09-13 03:10:41 +0530

    fix bug one

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     // PUBLIC CONFIGURATION (Exposed to frontend)
     // The Web App URL from Google Apps Script deployment
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxwKmCd-dAv5fpqTQ10VUEmsybGeWb33r9m3d6g5UJd3PGdwRvl1aanRJM1uCRaE4hkYA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxVd6G9yCWCBowAK2PZxWQLJRgJvff8y4wKh56fDUEnv3b_Sxoz3uva9kyZz-X-kEeG/exec",
 
     // Hostinger MySQL Database API URL (dual-write endpoint)
     DB_API_URL: "https://academiaclass.in/api/index.php",
